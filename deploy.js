5df903cf43137bd35900e33c3a5f156688d7dffe
hakunamoni <hakunamoni@gmail.com>
2021-12-10 01:09:11 +0800

    Build lottery skeleton

---
+++ deploy.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONIC,
+    'https://ropsten.infura.io/v3/c303c2e21a314ab6abd7a092e39c151c'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecoden })
+        .send({ from: accounts[0], gas: '1000000'});
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
